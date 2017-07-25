77fea808576a8f1a50534d138314c8b7656bfd19
redsock88 <athasket@gmail.com>
2017-07-25 03:24:22 -0400

    Add files via upload

---
+++ Person_fo.java
@@ -0,0 +1,13 @@
+package random;
+
+public class Person_fo {
+	
+	public List<PersonFoDetails> search(String forename){//input from the uim file
+		List<PersonFoDetails> list = new List<PersonFoDetails>();
+		
+		//use string to search the person table for people with that forename.
+		//store results as the variable "list".
+		
+		return list; //returns list back to the java bean for the uim file to use.
+	}
+}
